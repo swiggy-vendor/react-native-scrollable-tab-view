2d0f9b876b201d56e0a12b37bed4dc120be1e0ac
harpreetsingh1627484 <harpreet.singh4@swiggy.in>
2023-06-23 10:33:50 +0530

    Remove @react-native-community/viewpager and move to react-native-pager-view

--- index.js
+++ index.js
@@ -12,17 +12,15 @@ const {
   StyleSheet,
   InteractionManager,
 } = ReactNative;
-
-const ViewPagerAndroid = require('@react-native-community/viewpager');
+import PagerView from 'react-native-pager-view';
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('@react-native-community/viewpager');
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPager) :
+  Animated.createAnimatedComponent(PagerView) :
   undefined;
 
 const ScrollableTabView = createReactClass({
